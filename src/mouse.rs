8425892cae270bed5392fd3cccbac333f470a257
Leevi Honkonen <leevi.honkonen@gmail.com>
2024-03-10 02:18:08 +0200

    feat: only clicked position is exposed

--- src/mouse.rs
+++ src/mouse.rs
@@ -25,26 +25,30 @@ fn setup(mut commands: Commands){
 }
 
 fn my_cursor_system(
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
     mut mycoords: ResMut<MyWorldCoords>,
     // query to get the window (so we can read the current cursor position)
     q_window: Query<&Window, With<PrimaryWindow>>,
     // query to get camera transform
     q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
 ) {
-    // get the camera info and transform
-    // assuming there is exactly one main camera entity, so Query::single() is OK
-    let (camera, camera_transform) = q_camera.single();
-
-    // There is only one primary window, so we can similarly get it from the query:
-    let window = q_window.single();
-
-    // check if the cursor is inside the window and get its position
-    // then, ask bevy to convert into world coordinates, and truncate to discard Z
-    if let Some(world_position) = window.cursor_position()
+    if mouse_button_input.just_released(MouseButton::Left) {
+        info!("left mouse just released");
+        // get the camera info and transform
+        // assuming there is exactly one main camera entity, so Query::single() is OK
+        let (camera, camera_transform) = q_camera.single();
+        
+        // There is only one primary window, so we can similarly get it from the query:
+        let window = q_window.single();
+        
+        // check if the cursor is inside the window and get its position
+        // then, ask bevy to convert into world coordinates, and truncate to discard Z
+        if let Some(world_position) = window.cursor_position()
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
         .map(|ray| ray.origin.truncate())
-    {
-        mycoords.value = world_position;
-        
+        {
+            mycoords.value = world_position;
+            
+        }
     }
 }
